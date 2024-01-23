75ace05e0c11886891cf8a9903daf8d6ad3d6dd4
Jelle <jelle.wijma@gmail.com>
2024-01-23 12:56:12 +0100

    popup succes styling

--- includes/templates/web/index.php
+++ includes/templates/web/index.php
@@ -39,8 +39,22 @@
   </section>
 <?php endif; ?>
 
+
 <?php if ($success) { ?>
-  <p class="notification is-primary"><?= $success; ?></p>
+  <div class="absolute top-4 inset-x-[calc(50vw-192px)] flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md animate-duration-300 opacity-0" id="success-popup">
+    <div class="flex items-center justify-center w-12 bg-emerald-500">
+      <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z" />
+      </svg>
+    </div>
+
+    <div class="px-4 py-2 -mx-3">
+      <div class="mx-3">
+        <span class="font-semibold text-emerald-500 dark:text-emerald-400">Success</span>
+        <p class="text-sm text-gray-600"><?= $success; ?></p>
+      </div>
+    </div>
+  </div>
 <?php } ?>
 
 <section class="text-gray-600 body-font">
@@ -219,8 +233,10 @@
   </div>
   <div class="bg-gray-100">
     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-      <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+      <p class="text-gray-500 text-sm text-center sm:text-left">© 2024 —
+        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@florian</a>
+        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@jelle</a>
+        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@daan</a>
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
         <a class="text-gray-500">
@@ -249,3 +265,27 @@
     </div>
   </div>
 </footer>
+
+<style>
+  #success-popup {
+    animation: slide-right 3500ms forwards;
+  }
+
+  @keyframes slide-right {
+    0% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+
+    85.7% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+
+    100% {
+      opacity: 0;
+      transform: translateX(50%);
+      display: none;
+    }
+  }
+</style>
