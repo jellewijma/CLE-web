36f9f4f831c5f2b1e6c39c8c71cc5c330ddfe1a3
Jelle <jelle.wijma@gmail.com>
2024-01-24 09:40:07 +0100

    error popup styling done

--- includes/templates/web/index.php
+++ includes/templates/web/index.php
@@ -29,7 +29,7 @@
   </div>
 </header>
 
-<?php if (!empty($errors)) : ?>
+<!-- <?php if (!empty($errors)) : ?>
   <section class="content">
     <ul class="notification is-danger">
       <?php foreach ($errors as $error) : ?>
@@ -37,7 +37,25 @@
       <?php endforeach; ?>
     </ul>
   </section>
-<?php endif; ?>
+<?php endif; ?> -->
+
+<?php if (!empty($errors)) { ?>
+  <div class="absolute top-4 inset-x-[calc(50vw-192px)] flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-md animate-duration-300 opacity-0" id="success-popup">
+    <div class="flex items-center justify-center w-12 bg-red-500">
+      <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z" />
+      </svg>
+    </div>
+
+    <div class="px-4 py-2 -mx-3">
+      <div class="mx-3">
+        <?php foreach ($errors as $error) : ?>
+          <li><?= $error; ?></li>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  </div>
+<?php } ?>
 
 
 <?php if ($success) { ?>
